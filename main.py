36588c431dd8eb0d5f2864c82b9915f45e2a70bd
kulvirvirk <kvirkdev@gmail.com>
2020-03-06 01:37:44 +0000

    added the code example of function with argument

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #1. Create a say_hello() function; that prints hello five times
+#2. Creat a function with one argument first_name and print it with default last name
+# ==============================================================
 
-
+#1. Create a say_hello() function; that prints hello five times
 def say_hello():
   counter = 1
   while counter < 6:
@@ -8,4 +10,13 @@ def say_hello():
     counter += 1
 
 say_hello()
+print('--------------******--------------\n')
 
+#2. Creat a function with one argument first_name and print it with default last name
+def print_name(first_name):
+  print(first_name + " Singh")
+
+print_name("Ajit")
+print_name("Babbu")
+print_name("Kuku")
+print('--------------******--------------\n')
