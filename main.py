bdf1157cef59e657df14151665437da49e867dfd
kulvirvirk <kvirkdev@gmail.com>
2020-03-06 01:04:14 +0000

    Created a say_hello() function; that prints hello five times

---
+++ main.py
@@ -0,0 +1,11 @@
+#1. Create a say_hello() function; that prints hello five times
+
+
+def say_hello():
+  counter = 1
+  while counter < 6:
+    print(str(counter) + " Hello!")
+    counter += 1
+
+say_hello()
+
