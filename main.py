c9ca089474dce2be044fd52848fb4fe9a36e0d23
kulvirvirk <kvirkdev@gmail.com>
2020-03-08 04:47:48 +0000

    added Doc Strings to all the functions

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #1. Create a say_hello() function; that prints hello five times
-#2. Creat a function with one argument first_name and print it with default last name
-# ==============================================================
+#2. Create a function with one argument first_name and print it with default last name
+#3. Create a funciton with default parameter and print its values
+#4. add Doc Strings to add info to each function ==============================================================
 
 #1. Create a say_hello() function; that prints hello five times
+
 def say_hello():
+  '''Prints "Hello" 5 times'''
   counter = 1
   while counter < 6:
     print(str(counter) + " Hello!")
@@ -12,11 +15,24 @@ def say_hello():
 say_hello()
 print('--------------******--------------\n')
 
-#2. Creat a function with one argument first_name and print it with default last name
+#2. Create a function with one argument first_name and print it with default last name
 def print_name(first_name):
+  '''Prints first name and last name'''
   print(first_name + " Singh")
 
 print_name("Ajit")
 print_name("Babbu")
 print_name("Kuku")
 print('--------------******--------------\n')
+
+
+#3. Create a funciton with default parameter and print its values
+def print_country(country = 'USA'):
+  '''Prints country name'''
+  print('I am from ' + country)
+
+print_country('Mexico')
+print_country()
+print_country('India')
+print('--------------******--------------\n')
+
